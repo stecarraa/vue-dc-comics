b4d462ec8461dc3988b92563cc9deffbf7133835
stecarraa <stefano@caratozzolo.me>
2022-07-15 17:59:02 +0200

    feat: footer structure ok

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
   <MainHeader />
   <MainContent />
+  <MainFooter />
   </div>
 </template>
 
 <script>
 import MainHeader from './components/Header.vue'
 import MainContent from './components/Main.vue'
+import MainFooter from './components/Footer.vue'
 export default {
   name: 'App',
   components: {
     MainHeader,
-    MainContent
+    MainContent,
+    MainFooter
   }
 }
 </script>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,91 @@
 <template>
-    
+<div>
+    <div class="main-footer-container">
+        <div class="container p-3">
+            <div class="row d-flex h-100 align-items-center">
+                <div class="col-6 text-white d-flex mt-2">
+                    <div class="col-4">
+                                <h6>DC COMICS</h6>
+                                <ul class="list-unstyled">
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                </ul>
+                                <h6>SHOP</h6>
+                                <ul class="list-unstyled">
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                </ul>
+                            </div>
+                            <div class="col-4">
+                                <h6>DC COMICS</h6>
+                                <ul class="list-unstyled">
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                </ul>
+                            </div>
+                            <div class="col-4">
+                                <h6>DC COMICS</h6>
+                                <ul class="list-unstyled">
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                </ul>
+                            </div></div>
+                <div class="col-6 p-5 position-relative"><img class="position-absolute  top-50 start-50 translate-middle" src="../assets/img/dc-logo-bg.png" alt=""></div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid bg-black">
+        <div class="container">
+            
+              <div class="row d-flex align-items-center  justify-content-between p-5">
+            <div class="col-6">
+                <button class="btn btn-light">Sign-up NOW</button>
+            </div>
+            <div class="col-6"><ul class="list-unstyled d-flex justify-content-between">
+                                    <li><img src="../assets/img/footer-facebook.png" alt=""></li>
+                                    <li><img src="../assets/img/footer-periscope.png" alt=""></li>
+                                    <li><img src="../assets/img/footer-pinterest.png" alt=""></li>
+                                    <li><img src="../assets/img/footer-youtube.png" alt=""></li>
+                                    <li><img src="../assets/img/footer-youtube.png" alt=""></li>
+                                </ul></div>
+        </div>
+        </div>
+    </div>
+</div>
 </template> 
 
 <script>
 export default {
-    
+    name:'MainFooter'
 }
 </script>
 
 <style scoped lang="scss">
+.main-footer-container{
+    background-image: url('../assets/img/footer-bg.jpg');
+    background-size: cover;
+    height: 40vh;
+    background-repeat: no-repeat;
+}
 
+li{
+    font-weight: 100;
+}
 </style>
