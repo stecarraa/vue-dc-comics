fb8a9a1b69b8699200bd9be48b2327bd378d1356
stecarraa <stefano@caratozzolo.me>
2022-07-18 17:19:12 +0200

    feat : favico modified\product added dinamically by props

--- src/components/BlueBand.vue
+++ src/components/BlueBand.vue
@@ -3,13 +3,13 @@
         <div class="container-fluid ms-blue-bg">
             <div class="container">
                 <div class="row">
-                    <div class="col-12 d-flex text-white justify-content-center align-items-center text-uppercase p-4">
+                    <div class="col-12 d-flex text-white justify-content-center align-items-center text-uppercase p-5">
                        
-                            <div class="col text-center"><img class="w-25" src="../assets/img/buy-comics-digital-comics.png" alt=""> Digital Comics</div>
-                            <div class="col text-center"><img class="w-25" src="../assets/img/buy-comics-merchandise.png" alt="">dc merchandise</div>
-                            <div class="col text-center"><img class="w-25" src="../assets/img/buy-comics-subscriptions.png" alt="">subscription</div>
-                            <div class="col text-center"><img class="w-25" src="../assets/img/buy-comics-shop-locator.png" alt="">comic shop locator</div>
-                            <div class="col text-center"><img class="w-25" src="../assets/img/buy-dc-power-visa.svg" alt="">dc power visa</div>
+                            <div class="col text-center"><img class="w-5" src="../assets/img/buy-comics-digital-comics.png" alt=""> Digital Comics</div>
+                            <div class="col text-center"><img class="w-5" src="../assets/img/buy-comics-merchandise.png" alt=""> dc merchandise</div>
+                            <div class="col text-center"><img class="w-5" src="../assets/img/buy-comics-subscriptions.png" alt=""> subscription</div>
+                            <div class="col text-center"><img class="w-5" src="../assets/img/buy-comics-shop-locator.png" alt=""> comic shop locator</div>
+                            <div class="col text-center"><img class="w-5" src="../assets/img/buy-dc-power-visa.svg" alt=""> dc power visa</div>
                             
                      
                     </div>
@@ -28,6 +28,10 @@ export default {
 <style lang="scss" scoped>
 .ms-blue-bg{
     background-color: #0281FA;
+}
+
+.w-5{
+    width: calc(100% / 5);
 }
 
 </style>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card">
+
+    <div class="comic-img-container">
+        <img :src="thumbnail" :alt="series">
+    </div>
+    <h4>{{ series }}</h4>
+
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        thumbnail: String,
+        price: String,
+        series: String,
+        type: String,
+    }
+}
+</script>
+
+<style>
+
+</style>
